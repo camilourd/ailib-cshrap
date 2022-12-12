ccead4521fe6c328fe60bdf0c95a6d4c3a5c884b
camilourd <camilourd@gmail.com>
2022-12-12 18:36:24 +0200

    Shuffle

---
+++ ailib/ailib/random/Shuffle.cs
@@ -0,0 +1,22 @@
+﻿namespace ailib.random
+{
+    public class Shuffle<T>
+    {
+        private RandomGenerator<double> generator;
+
+        public Shuffle(RandomGenerator<double> generator)
+        {
+            this.generator = generator;
+        }
+
+        public void apply(T[] values) {
+            for (int i = 0; i < values.Length; i++)
+            {
+                int next = (int)(values.Length * this.generator.next());
+                T temp = values[i];
+                values[i] = values[next];
+                values[next] = temp;
+            }
+        }
+    }
+}
